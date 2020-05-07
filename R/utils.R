af675659c73453f44662bfa5db698228cb707f81
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2020-05-07 20:21:22 +0200

    removed even more 'require' usages, added one utility function

--- R/utils.R
+++ R/utils.R
@@ -46,13 +46,17 @@ install_AmpGramModel <- function() {
   install_github("michbur/AmpGramModel")
 }
 
+is_AmpGramModel_installed <- function() {
+  "AmpGramModel" %in% rownames(installed.packages())
+}
+
 require_AmpGramModel <- function() {
-  if (!suppressWarnings(require("AmpGramModel", quietly = TRUE)) && !getOption("AmpGram_suppress_prompt")) {
+  if (!is_AmpGramModel_installed()) && !getOption("AmpGram_suppress_prompt")) {
     response <- menu(c("yes", "no", "no and don't ask me anymore"), 
                      title = "To be able to use AmpGram properly, you should have installed 'AmpGramModel' package available via GitHub. Install?")
     switch (response, 
             tryCatch(install_AmpGramModel(),
-               finally = if (!suppressWarnings(require("AmpGramModel"))) 
+               finally = if (!is_AmpGramModel_installed()) 
                  warning("There was an error during an attempt to install 'AmpGramModel' package.", call. = FALSE)),
             warning("You cannot access full functionality of this package without having installed 'AmpGramModel'. You can do it manually by calling 'devtools::install_github('michbur/AmpGramModel')'", call. = FALSE),
             {options(AmpGram_suppress_prompt = TRUE)
--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   options(AmpGram_suppress_prompt = FALSE)
-  if ("AmpGramModel" %in% rownames(installed.packages()))
+  if (!is_AmpGramModel_installed())
     warning("To be able to use AmpGram properly, you should install 'AmpGramModel' package available via GitHub. You can do it by calling 'install_AmpGramModel()'")
 }
