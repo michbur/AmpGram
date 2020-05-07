f4db39f50f0976f7bc0ca708512fa613c8d8833c
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2020-05-07 20:13:55 +0200

    fixed require in package

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   options(AmpGram_suppress_prompt = FALSE)
-  if (!suppressWarnings(require("AmpGramModel"))) 
+  if ("AmpGramModel" %in% rownames(installed.packages()))
     warning("To be able to use AmpGram properly, you should install 'AmpGramModel' package available via GitHub. You can do it by calling 'install_AmpGramModel()'")
 }
