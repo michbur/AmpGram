42c2899348e56f21cdb5bef87664f9a593859f24
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2020-05-07 20:24:44 +0200

    improving checking AmpGramModel

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   options(AmpGram_suppress_prompt = FALSE)
-  if ("AmpGramModel" %in% rownames(installed.packages()))
+  check_AmpGramModel <- try(find.package("AmpGramModel"), silent = TRUE)
+  if (inherits(check_AmpGramModel, "try-error"))
     warning("To be able to use AmpGram properly, you should install 'AmpGramModel' package available via GitHub. You can do it by calling 'install_AmpGramModel()'")
 }
