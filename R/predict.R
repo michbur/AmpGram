94f21cde180ada52ab7e9545e92d83095ee6b76a
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2020-05-07 20:01:20 +0200

    added more words

--- R/predict.R
+++ R/predict.R
@@ -34,48 +34,10 @@ predict.ampgram_model <- function(object, newdata, ...) {
     all_mers_pred <- predict(object[["rf_mers"]], ngram_count)[["predictions"]][, 2]
     single_prot_pred <- predict(object[["rf_peptides"]], 
                                 calculate_statistics(all_mers_pred))[["predictions"]][, 2]
+    res <- list(seq = ith_seq,
+                all_mers_pred = all_mers_pred,
+                single_prot_pred = single_prot_pred)
+    
+    class(res) <- "single_ampgram_pred"
   })
-  
 }
-
-count_longest <- function(x) {
-  splitted_x <- strsplit(x = paste0(as.numeric(x > 0.5), collapse = ""),
-                         split = "0")[[1]]
-  len <- unname(sapply(splitted_x, nchar))
-  if (length(len[len > 0]) == 0) {
-    0 } else {
-      len[len > 0]
-    }
-}
-
-calculate_statistics <- function(pred) {
-  data.frame(fraction_true = mean(pred > 0.5),
-             pred_mean = mean(pred),
-             pred_median = median(pred),
-             n_peptide = length(pred),
-             n_pos = sum(pred > 0.5),
-             pred_min = min(pred),
-             pred_max = max(pred), 
-             longest_pos = max(count_longest(pred)),
-             n_pos_10 = sum(count_longest(pred) >= 10),
-             frac_0_0.2 = sum(pred <= 0.2)/length(pred),
-             frac_0.2_0.4 = sum(pred > 0.2 & pred <= 0.4)/length(pred),
-             frac_0.4_0.6 = sum(pred > 0.4 & pred <= 0.6)/length(pred),
-             frac_0.6_0.8 = sum(pred > 0.6 & pred <= 0.8)/length(pred),
-             frac_0.8_1 = sum(pred > 0.8 & pred <= 1)/length(pred)) 
-}
-
-find_ngrams <- function(seq, decoded_ngrams) {
-  
-  end_pos <- 10L:length(seq)
-  start_pos <- end_pos - 9
-  
-  res <- binarize(do.call(rbind, lapply(1L:length(end_pos), function(ith_mer_id) {
-    ten_mer <- paste0(seq[start_pos[ith_mer_id]:end_pos[ith_mer_id]], collapse = "")
-    stri_count(ten_mer, regex = decoded_ngrams)
-  })))
-  
-  res
-}
-
-
