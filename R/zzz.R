f316c225cdc217606c0bf0bc8c08552ac1847266
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2020-05-09 13:42:36 +0200

    fixing startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-.onLoad <- function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) {
   options(AmpGram_suppress_prompt = FALSE)
   if (!is_AmpGramModel_installed())
     packageStartupMessage("To be able to use AmpGram properly, you should install 'AmpGramModel' package available via GitHub. You can do it by calling 'install_AmpGramModel()'")
