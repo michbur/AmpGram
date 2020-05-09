5351f54ed245970b3095e28c6eeb597efea4c7cc
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2020-05-09 08:48:40 +0200

    updated menu

--- R/utils.R
+++ R/utils.R
@@ -58,17 +58,22 @@ is_AmpGramModel_installed <- function() {
 }
 
 require_AmpGramModel <- function() {
-  if (!is_AmpGramModel_installed() && !getOption("AmpGram_suppress_prompt")) {
-    response <- menu(c("yes", "no", "no and don't ask me anymore"), 
-                     title = "To be able to use AmpGram properly, you should have installed 'AmpGramModel' package available via GitHub. Install?")
-    switch (response, 
-            tryCatch(install_AmpGramModel(),
-               finally = if (!is_AmpGramModel_installed()) 
-                 warning("There was an error during an attempt to install 'AmpGramModel' package.", call. = FALSE)),
-            warning("You cannot access full functionality of this package without having installed 'AmpGramModel'. You can do it manually by calling 'devtools::install_github('michbur/AmpGramModel')'", call. = FALSE),
-            {options(AmpGram_suppress_prompt = TRUE)
-              cat("Ok, but you cannot access full functionality of this package without having installed 'AmpGramModel'")},
-            warning("You cannot access full functionality of this package without having installed 'AmpGramModel'. You can do it manually by calling 'devtools::install_github('michbur/AmpGramModel')'", call. = FALSE)
-            )
+  if(interactive()) {
+    if (!is_AmpGramModel_installed() && !getOption("AmpGram_suppress_prompt")) {
+      response <- menu(c("yes", "no", "no and don't ask me anymore"), 
+                       title = "To be able to use AmpGram properly, you should have installed 'AmpGramModel' package available via GitHub. Install?")
+      switch (response, 
+              tryCatch(install_AmpGramModel(),
+                       finally = if (!is_AmpGramModel_installed()) 
+                         warning("There was an error during an attempt to install 'AmpGramModel' package.", call. = FALSE)),
+              warning("You cannot access full functionality of this package without having installed 'AmpGramModel'. You can do it manually by calling 'devtools::install_github('michbur/AmpGramModel')'", call. = FALSE),
+              {options(AmpGram_suppress_prompt = TRUE)
+                cat("Ok, but you cannot access full functionality of this package without having installed 'AmpGramModel'")},
+              warning("You cannot access full functionality of this package without having installed 'AmpGramModel'. You can do it manually by calling 'devtools::install_github('michbur/AmpGramModel')'", call. = FALSE)
+      )
+    } 
+  } else {
+    message("To be able to use AmpGram properly, you should have installed 'AmpGramModel' with 'devtools::install_github('michbur/AmpGramModel'.")
   }
+  
 }
