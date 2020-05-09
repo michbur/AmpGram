6b6f234e3359d000aea0e2d7ffe24b4b9158c2e7
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2020-05-09 13:32:54 +0200

    fixing message

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   options(AmpGram_suppress_prompt = FALSE)
   if (!is_AmpGramModel_installed())
-    message("To be able to use AmpGram properly, you should install 'AmpGramModel' package available via GitHub. You can do it by calling 'install_AmpGramModel()'")
+    packageStartupMessage("To be able to use AmpGram properly, you should install 'AmpGramModel' package available via GitHub. You can do it by calling 'install_AmpGramModel()'")
 }
